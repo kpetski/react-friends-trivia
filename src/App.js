c1579a633d62202357ab2bcc93a47309a033af98
PETSKI, KERI <keripetski@northwesternmutual.com>
2018-07-07 14:15:12 -0500

    adding git pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { DB_CONFIG } from './config/config'
+import { DB_CONFIG, adminPassword} from './config/config'
 import firebase from '../node_modules/firebase/app'
 import '../node_modules/firebase/database'
 
@@ -11,7 +11,10 @@ class App extends Component {
     this.app = firebase.initializeApp(DB_CONFIG)
     this.database = this.app.database().ref().child('questions')
     this.state = {
-      questions: []
+      questions: [],
+      isAdmin: false,
+      questionRefKey: null,
+      showAlerts: true
     }
   }
 
@@ -33,11 +36,10 @@ class App extends Component {
     const QuestionJSX =
       <div>
         {this.state.questions.map((question, index) => {
-          console.log(question.question)
           return (
             <div key={index}>
               <h3>{question.question}</h3>
-              {question.options.map((option, index) => {
+              {question.options && question.options.map((option, index) => {
                 return <button className='btn-primary' key={index}>{`${option}`}</button>
               })}
             </div>
@@ -47,20 +49,76 @@ class App extends Component {
     return QuestionJSX
   }
 
+  addQuestion = (event) => {
+    event.preventDefault()
+    var newRef = this.database.push({
+      "answer" : document.getElementById('answerInput').value,
+      "options" : [ document.getElementById('questionInput').value, 
+        document.getElementById('incorrect1').value ? document.getElementById('incorrect1').value : 'AddAnswerHere',
+        document.getElementById('incorrect2').value ? document.getElementById('incorrect2').value : 'AddAnswerHere',
+        document.getElementById('incorrect3').value ? document.getElementById('incorrect3').value : 'AddAnswerHere'],
+      "question" : document.getElementById('questionInput').value
+    })
+    this.setState({questionRefKey :newRef.key,
+      showAlerts: true})
+  }
+
   render () {
     return (
       <div>
         <header className='App App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Friends Trivia</h1>
+          <h1 className='App-title'>{this.state.isAdmin ? 'Admin' : 'Friends Trivia'}</h1>
         </header>
         <br />
+        {this.state.isAdmin === false &&
         <div className='Body'>
           {this.state.questions[0] && this.getQuestion()}
         </div>
+        }
+        {this.state.isAdmin &&
+        <div className='Body'>
+          <form id='adminForm' onSubmit={this.addQuestion}>
+            <div className='form-group'>
+              <h2>Add New Question</h2>
+              <label htmlFor="questionInput">Question:</label>
+              <input type='text' className='form-control' id='questionInput' placeholder='Question' required />
+              <label htmlFor="answerInput">Answer:</label>
+              <input type='text' className='form-control' id='answerInput' placeholder='Answer' required/>
+              <br />
+              <h2>Other Incorrect Answer Options</h2>
+              <input type='text' className='form-control' id='incorrect1' placeholder='incorrect answer 1' />
+              <input type='text' className='form-control' id='incorrect2' placeholder='incorrect answer 2' />
+              <input type='text' className='form-control' id='incorrect3' placeholder='incorrect answer 3' />
+              <br />
+              <button type='submit' className='btn btn-primary'>Add</button>
+            </div>
+          </form>
+          {this.state.questionRefKey && this.state.showAlerts && <div className="alert alert-success alert-dismissible">
+            <a href="#" className="close" data-dismiss="alert" aria-label="close" onClick={() => this.setState({showAlerts: false})}>&times;</a>
+            {`Success! Question has been inserted with ID:  ${this.state.questionRefKey}`}
+          </div>}
+
+        </div>
+        }
+        <footer className='App App-footer'>
+          <button className='btn-default' onClick={() => {
+            if(this.state.isAdmin === false)
+            {let password = window.prompt("Please Enter Password","")
+            if(password === adminPassword) {
+              this.setState({isAdmin: !this.state.isAdmin})}
+            } else {
+              this.setState({isAdmin: !this.state.isAdmin})
+            }}}>Admin</button>
+        </footer>
       </div>
     )
   }
 }
 
 export default App
+
+// onClick={() => this.setState({showAlerts: false})}
+// <button className='btn-default' onClick={() => this.setState({isAdmin: !this.state.isAdmin})}>Admin</button>
+      
+
